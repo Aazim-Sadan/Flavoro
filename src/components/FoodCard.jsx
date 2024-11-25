acf93a0daabda2eeea18c8a4f11ade8061b5ffb1
Aazim Sadan <aazimsadan99@gmail.com>
2024-11-26 00:35:38 +0530

    create cart

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { FaStar } from "react-icons/fa";
 
-const FoodCard = () => {
+const FoodCard = ({id, name, price, desc, img, rating}) => {
     return (
         <div className=' font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2'>
-            <img src="https://w7.pngwing.com/pngs/133/54/png-transparent-pancakes-served-on-plate-banana-pancakes-scrambled-eggs-breakfast-ihop-pancake-food-recipe-eating-thumbnail.png"
+            <img src={img}
                 alt=""
                 className='w-auto h-[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out'
             />
             <div className='text-sm flex justify-between '>
-                <h2>Onian Pizza </h2>
-                <span className='text-green-500'>₹130</span>
+                <h2>{name}</h2>
+                <span className='text-green-500'>₹{price}</span>
             </div>
-            <p className='text-sm font-normal'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fugit.
-            </p>
+            <p className='text-sm font-normal'> {desc.slice(0, 50)}... </p>
             <div className=' flex justify-between'>
                 <span className='flex justify-center items-center'>
-                    <FaStar className='mr-1 text-yellow-400' /> 4.5
+                    <FaStar className='mr-1 text-yellow-400' /> {rating}
                 </span>
                 <button className='p-1 text-white bg-green-500 hover:bg-gre rounded-lg text-sm '> Add to cart</button>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import CategoryMenu from '../components/CategoryMenu'
 import FoodItems from '../components/FoodItems'
+import Cart from '../components/Cart'
 
 const Home = () => {
   return (
-   <>
-  <Navbar/>
-  <CategoryMenu/>
-  <FoodItems/>
-  </>
+    <>
+      <Navbar />
+      <CategoryMenu />
+      <FoodItems />
+      <Cart />
+    </>
   )
 }
 
