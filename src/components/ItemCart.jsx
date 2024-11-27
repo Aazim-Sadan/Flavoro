c223396ae2aa185cc192e4523d3ffe0f8c0c8500
Aazim Sadan <aazimsadan99@gmail.com>
2024-11-27 22:42:02 +0530

    fixing price in cart

--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -26,7 +26,7 @@ const ItemCart = ({ id, name, img, price, qty }) => {
             <div className='leading-7'>
                 <h2 className='font-bold text-gray-800'>{name}</h2>
                 <div className='flex justify-between'>
-                    <span className='text-green-500 font-bold'>₹{price}</span>
+                    <span className='text-green-500 font-bold'>₹{price*qty}</span>
                     <div className='flex justify-center items-center gap-2 absolute right-7'>
                         <AiOutlineMinus onClick={() => qty > 1 ? dispatch(decrementQty({ id })) : (qty = 0)} className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none  rounded-md p-1 text-xl transition-all ease-linear cursor-pointer' />
                         <span>{qty}</span>
